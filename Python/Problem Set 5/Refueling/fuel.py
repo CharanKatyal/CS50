56d0644646681b14bfb6a842978fc39991e0682d
Charan Katyal <chessmyfriend@gmail.com>
2025-09-23 21:21:52 +0530

    Create fuel.py

---
+++ Python/Problem Set 5/Refueling/fuel.py
@@ -0,0 +1,40 @@
+# fuel.py - By Charan Katyal
+
+def main():
+    while True:
+        try:
+            fraction = input("Fraction: ")
+            percentage = convert(fraction)
+            print(gauge(percentage))
+            break
+        except (ValueError, ZeroDivisionError):
+            pass
+
+
+def convert(fraction):
+    try:
+        x, y = fraction.split("/")
+        x = int(x)
+        y = int(y)
+        if y == 0:
+            raise ZeroDivisionError
+        if x < 0 or y < 0 or x > y:
+            raise ValueError
+        percentage = round((x / y) * 100)
+        return percentage
+    except (ValueError, ZeroDivisionError):
+        raise
+
+
+def gauge(percentage):
+    if percentage <= 1:
+        return "E"
+    elif percentage >= 99:
+        return "F"
+    else:
+        return f"{percentage}%"
+
+
+if __name__ == "__main__":
+    main()
+
